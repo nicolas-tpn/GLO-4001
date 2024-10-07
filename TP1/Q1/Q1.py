979e8094c3e34d158e9425921a0e432f0677852b
Nicolas Taupin <62623511+Saucisse2toulouse@users.noreply.github.com>
2024-10-07 12:24:06 -0400

    avancée exercice 2 (pas fonctionnel)

--- TP1/Q1/Q1.py
+++ TP1/Q1/Q1.py
@@ -1,35 +1,36 @@
 import scipy.io as sc
 import numpy as np
 import matplotlib.pyplot as plt
-data = sc.loadmat('Q1Donnees.mat')
-data2 = sc.loadmat('Carte.mat')
 
-z = data['z'].flatten()
-t = data['t'].flatten()
-g = data['g'].flatten()
-Carte = data2['Carte']
+data = sc.loadmat("Q1Donnees.mat")
+data2 = sc.loadmat("Carte.mat")
+
+z = data["z"].flatten()
+t = data["t"].flatten()
+g = data["g"].flatten()
+Carte = data2["Carte"]
 
 # Question 1.1 : Calibration
 
 t_upper_bound = 12.87
 t_lower_bound = 2.86
-K1 = np.mean(g[t<2.75])
-g_moy = np.mean(g[(t>t_lower_bound) & (t<t_upper_bound)])
-delta_t = t_upper_bound-t_lower_bound
-angular_speed_moy = 2*np.pi/delta_t
-K2 = (g_moy-K1)/angular_speed_moy
+K1 = np.mean(g[t < 2.75])
+g_moy = np.mean(g[(t > t_lower_bound) & (t < t_upper_bound)])
+delta_t = t_upper_bound - t_lower_bound
+angular_speed_moy = 2 * np.pi / delta_t
+K2 = (g_moy - K1) / angular_speed_moy
 print("K1", K1, "K2", K2)
 
 # Question 1.2 : Carte 2D locale
 
 points = np.empty((0, 2))
 current_angle = 0
-for index, timing in enumerate(t[(t>t_lower_bound) & (t<t_upper_bound)]):
-# for index, timing in enumerate(t):
+for index, timing in enumerate(t[(t > t_lower_bound) & (t < t_upper_bound)]):
+    # for index, timing in enumerate(t):
     # Faire un truc plus propre pour le delta_t que 0.05
-    current_angle += (g[index]-K1)/K2 * 0.05
-    x = np.cos(current_angle)*z[index]
-    y = np.sin(current_angle)*z[index]
+    current_angle += (g[index] - K1) / K2 * 0.05
+    x = np.cos(current_angle) * z[index]
+    y = np.sin(current_angle) * z[index]
     point = np.array([[x, y]])
 
     points = np.append(points, point, axis=0)
@@ -47,35 +48,45 @@ radian_angle = np.radians(angle)
 translation_x = 11
 translation_y = 5.8
 
-adjusted_points = np.dot(adjusted_points,np.array([[np.cos(radian_angle),-np.sin(radian_angle),0,0],[np.sin(radian_angle),np.cos(radian_angle),0,0],[0,0,1,0],[0,0,0,1]]))
-adjusted_points = np.dot(adjusted_points,np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[translation_x,translation_y,0,1]]))
+adjusted_points = np.dot(
+    adjusted_points,
+    np.array(
+        [
+            [np.cos(radian_angle), -np.sin(radian_angle), 0, 0],
+            [np.sin(radian_angle), np.cos(radian_angle), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ]
+    ),
+)
+adjusted_points = np.dot(
+    adjusted_points,
+    np.array(
+        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [translation_x, translation_y, 0, 1]]
+    ),
+)
 
-plt.plot(Carte[0,:],Carte[1,:],'b')
-plt.plot(points[:,0], points[:, 1],'r')
-plt.plot(adjusted_points[:,0], adjusted_points[:, 1],'r')
-plt.axis('equal')
+plt.plot(Carte[0, :], Carte[1, :], "b")
+plt.plot(points[:, 0], points[:, 1], "r")
+plt.plot(adjusted_points[:, 0], adjusted_points[:, 1], "r")
+plt.axis("equal")
 plt.show()
 
 
-plt.subplot(2,1,1)
+plt.subplot(2, 1, 1)
 plt.plot(t, z, linewidth=2)
-plt.ylabel('Mesure telemetre (V)')
-plt.subplot(2,1,2)
+plt.ylabel("Mesure telemetre (V)")
+plt.subplot(2, 1, 2)
 plt.plot(t, g, linewidth=2)
-plt.ylabel('Mesure gyroscope (V)')
-plt.xlabel('Temps (s)')
+plt.ylabel("Mesure gyroscope (V)")
+plt.xlabel("Temps (s)")
 plt.tight_layout()
 plt.show()
 
-plt.plot(Carte[0,:],Carte[1,:],'b')
-plt.plot(points[:,0], points[:, 1],'r')
-plt.xlabel('Coordonnées en x (m)')
-plt.ylabel('Coordonnées en y (m)')
-plt.axis('equal')
-plt.savefig('environnement.png')
+plt.plot(Carte[0, :], Carte[1, :], "b")
+plt.plot(points[:, 0], points[:, 1], "r")
+plt.xlabel("Coordonnées en x (m)")
+plt.ylabel("Coordonnées en y (m)")
+plt.axis("equal")
+plt.savefig("environnement.png")
 plt.show()
-
-
-
-
-
--- TP1/Q2/Q2.py
+++ TP1/Q2/Q2.py
@@ -4,32 +4,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Question 2.1 : Génération d'une image
-L1 = np.array([-0.4,0,2.4])
-L2 = np.array([0,0,2])
-L3 = np.array([0.4,0,2.4])
+L1 = np.array([-0.4, 0, 2.4])
+L2 = np.array([0, 0, 2])
+L3 = np.array([0.4, 0, 2.4])
 
-def reprojection(H,focale,L):
-    # On considère que la pose que l'on a est de la forme (x,z,theta)
-    matrice_intrinseque = np.array([[1,0,0,0],[0,1,0,0],[0,0,1/focale,0]])
+
+def reprojection(H, focale, L):
+
+    # Passage des points en coordonnées homogènes
+
+    if L.ndim == 1:
+        L = L.reshape(1, -1)
+    L = np.hstack([L, np.ones((L.shape[0], 1))])
+
+    # On considère que la pose que l'on a est de la forme (x,y,z,theta)
+    matrice_intrinseque = np.array(
+        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 1 / focale, 0]]
+    )
     x = H[0]
-    z = H[1]
-    theta = H[2]
-    matrice_extrinseque = np.array([
-        [np.cos(theta),0,np.sin(theta),-x],
-        [0,1,0,0],
-        [-np.sin(theta),0,np.cos(theta),-z],
-        [0,0,0,1]])
-    
-    C = np.array([])
-    
+    y = H[1]
+    z = H[2]
+    theta = H[3]
+    matrice_extrinseque = np.array(
+        [
+            [np.cos(theta), 0, np.sin(theta), -x],
+            [0, 1, 0, -y],
+            [-np.sin(theta), 0, np.cos(theta), -z],
+            [0, 0, 0, 1],
+        ]
+    )
+
+    C = np.empty((0, 4))
+
     for Li in L:
-        C.append(matrice_extrinseque@matrice_intrinseque@Li)
+        new_line = matrice_extrinseque @ matrice_intrinseque @ Li
+        new_line = new_line.reshape(1, -1)
+        C = np.vstack([C, new_line])
 
     return C
+
 
 # Question 2.2 : Localisation par la minimisation de l'erreur de reprojection
 
 
+def somme_des_residuels_au_carre(pose_camera, focale, L, C):
+    # pose_camera de la forme : [x,z,theta]
+    # L contient les points dans le repère monde
+    # C contient les points dans le repère caméra
 
-L = np.array([L1,L2,L3])
-print(reprojection())
+    somme = 0
+
+    for index, Li in enumerate(L):
+        somme += (np.abs(C[index] - reprojection(pose_camera, focale, Li))) ** 2
+
+    return somme
+
+
+# On décale légèrement la pose par rapport à la pose initiale
+pose_initiale_camera = [0.2, 0, 0.19, 0.21]
+focale = 1500
+L = np.array([L1, L2, L3])
+C = reprojection(pose_initiale_camera, focale, L)
+
+pose_solution = fmin(
+    somme_des_residuels_au_carre,
+    pose_initiale_camera,
+    args=(focale, L, C),
+    maxiter=1000,
+)
+
+print(pose_solution)
